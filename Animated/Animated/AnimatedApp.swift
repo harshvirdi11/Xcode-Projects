fd4731dd2cd28e64ecc79b70a9c26a8652e502e3
HarshVirdi <harshvirdi11@gmail.com>
2026-01-30 16:32:51 +0530

    Animation

---
+++ Animated/Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Harsh Virdi on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
