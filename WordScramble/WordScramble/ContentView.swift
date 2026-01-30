12a17db486dec1ef70e457039f85f223fd8d5a52
HarshVirdi <harshvirdi11@gmail.com>
2026-01-30 12:29:43 +0530

    Feature Update: Score addition, restricted answer minimum length

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -14,33 +14,64 @@ struct ContentView: View {
     @State private var errorTitle: String = ""
     @State private var errorMessage: String = ""
     @State private var showingError: Bool = false
+    @State private var score: Int = 0
     var body: some View {
-        NavigationStack{
-            List{
-                Section{
-                    TextField("Enter you word", text: $newWord)
-                        .textInputAutocapitalization(.never)
+            NavigationStack{
+                List{
+                    Section{
+                        TextField("Enter you word", text: $newWord)
+                            .textInputAutocapitalization(.never)
+                        
+                    }
+                    .listRowBackground(Color.white.opacity(0.5))
+                    Section{
+                        ForEach(usedWords, id: \.self){ word in
+                            HStack{
+                                Text("\(word.count)")
+                                    .font(.caption.bold())
+                                    .foregroundStyle(.white)
+                                    .padding(8)
+                                    .background(.blue.gradient.opacity(0.8))
+                                    .clipShape(Circle())
+                                    .shadow(radius: 2)
+                                Text(word)
+                            }
+                        }
+                        
+                    }
+                    .listRowBackground(Color.white.opacity(0.5))
                 }
-                Section{
-                    ForEach(usedWords, id: \.self){ word in
-                        HStack{
-                            Image(systemName: "\(word.count).circle")
-                            Text(word)
+                .navigationTitle(rootWord)
+                .scrollContentBackground(.hidden)
+                .background(LinearGradient(colors: [.blue, .purple],
+                                              startPoint: .topLeading,
+                                              endPoint: .bottomTrailing)
+                                              .ignoresSafeArea())
+                .toolbar{
+                    ToolbarItem(){
+                        Button{
+                            startGame()
+                        } label: {
+                            Text("New Word")
+                                .foregroundStyle(.blue)
+                                .fontWeight(.medium)
                         }
                     }
+                    
+                    ToolbarItem(placement: .principal){
+                        Text("Score: \(score)")
+                            .font(Font.headline.bold())
+                    }
+                }
+                .onSubmit(addNewWord)
+                .onAppear(perform: startGame)
+                .alert(errorTitle, isPresented: $showingError)
+                {
+                    Button("OK"){}
+                } message: {
+                    Text(errorMessage)
                 }
             }
-            .navigationTitle(rootWord)
-            .onSubmit(addNewWord)
-            .onAppear(perform: startGame)
-            .alert(errorTitle, isPresented: $showingError)
-            {
-                Button("OK"){}
-            } message: {
-                Text(errorMessage)
-            }
-        }
-        
     }
     
     func addNewWord() {
@@ -58,6 +89,16 @@ struct ContentView: View {
             return
         }
         
+       guard tooShort(word: answer) else {
+            wordError(title: "Can't do that!", message: "Word is too short")
+           return
+        }
+        
+        guard sameWord(word: answer) else {
+            wordError(title: "Can't do that!", message: "You can't use the same word given above!")
+            return
+        }
+        
         guard isReal(word: answer) else {
             wordError(title: "Can't do that!", message: "\(newWord) is not a real word!")
             return
@@ -66,10 +107,14 @@ struct ContentView: View {
         withAnimation{
             usedWords.insert(answer, at: 0)
         }
+        score += answer.count + 10
         newWord = ""
     }
     
     func startGame() {
+        score = 0
+        newWord = ""
+        usedWords.removeAll()
         if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
             if let startWords = try? String(contentsOf: startWordsUrl, encoding: .utf8)
             {
@@ -96,6 +141,7 @@ struct ContentView: View {
         }
         return true
     }
+    
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0 , length: word.utf16.count)
@@ -103,14 +149,20 @@ struct ContentView: View {
         return misspelledRange.location == NSNotFound
     }
     
+    func tooShort(word: String) -> Bool {
+        return word.count >= 3
+    }
+    
+    func sameWord(word: String) -> Bool {
+        return word != rootWord
+    }
+    
     func wordError(title: String, message: String){
         errorTitle = title
         errorMessage = message
         showingError = true
     }
-    
 }
-
 
 #Preview {
     ContentView()
