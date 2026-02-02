ade99c1c10f0758fe5d06716554680cdd59d4243
HarshVirdi <harshvirdi11@gmail.com>
2026-02-02 15:23:00 +0530

    Style Changes

--- Animated-2/Animated-2/ContentView.swift
+++ Animated-2/Animated-2/ContentView.swift
@@ -18,23 +18,45 @@ struct ContentView: View {
     let possibleColors: [Color] = [.blue, .red, .green, .yellow, .purple, .pink, .orange, .brown]
     
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<snake.count, id: \.self) { num in
-                Text(String(snake[num]))
-                    .font(Font.headline)
-                    .background(snakeColor)
-                    .offset(dragAmount)
-                    .animation(.linear.delay(Double(num)/20.0), value: dragAmount)
+        ZStack{
+            
+            LinearGradient(colors: [.yellow,.red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+                .opacity(0.8)
+            VStack{
+                Text("Snakey needs to be dragged!")
+                    .font(Font.title.bold())
+                    .frame(width: 400, height: 100)
+                   
+                Spacer()
+                
+                HStack(spacing: 0) {
+                    ForEach(0..<snake.count, id: \.self) { num in
+                        Text(String(snake[num]))
+                            .foregroundStyle(.white)
+                            .font(.title2)
+                            /*.padding([.leading, .trailing], 4)
+                            .padding([.top, .bottom], 2)
+                             */
+                            .frame(width: 20, height: 35)
+                            .background(snakeColor)
+                            .cornerRadius(5)
+                            .offset(dragAmount)
+                            .animation(.linear.delay(Double(num)/20.0), value: dragAmount)
+                    }
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged{dragAmount = $0.translation}
+                        .onEnded { _ in dragAmount = .zero
+                            snakeColor = possibleColors.randomElement() ?? .blue
+                        }
+                    
+                )
+                Spacer()
+                Spacer()
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged{dragAmount = $0.translation}
-                .onEnded { _ in dragAmount = .zero
-                    snakeColor = possibleColors.randomElement() ?? .blue
-                }
-                          
-            )
     }
 }
 
