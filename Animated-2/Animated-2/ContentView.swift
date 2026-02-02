1481d24088eca2ea503d74cb3dfbd1a730132f91
HarshVirdi <harshvirdi11@gmail.com>
2026-02-02 14:48:59 +0530

    Color snake initial

--- Animated-2/Animated-2/ContentView.swift
+++ Animated-2/Animated-2/ContentView.swift
@@ -11,15 +11,18 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let snake = Array("Hello World!")
+    let snake = Array("Long Snake sssssss")
     @State private var dragAmount: CGSize = .zero
     @State private var enabled: Bool = false
+    @State private var snakeColor: Color = .blue
+    let possibleColors: [Color] = [.blue, .red, .green, .yellow, .purple, .pink, .orange, .brown]
+    
     var body: some View {
         HStack(spacing: 0) {
             ForEach(0..<snake.count, id: \.self) { num in
                 Text(String(snake[num]))
                     .font(Font.headline)
-                    .background(enabled ? Color.red : Color.blue)
+                    .background(snakeColor)
                     .offset(dragAmount)
                     .animation(.linear.delay(Double(num)/20.0), value: dragAmount)
             }
@@ -28,7 +31,7 @@ struct ContentView: View {
             DragGesture()
                 .onChanged{dragAmount = $0.translation}
                 .onEnded { _ in dragAmount = .zero
-                    enabled.toggle()
+                    snakeColor = possibleColors.randomElement() ?? .blue
                 }
                           
             )
