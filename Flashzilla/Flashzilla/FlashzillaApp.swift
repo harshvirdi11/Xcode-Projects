489758c430abf534751ac8ed95a2ab4a8de9121b
HarshVirdi <harshvirdi11@gmail.com>
2026-05-08 10:47:50 +0530

    no message

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Harsh Virdi on 18/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
